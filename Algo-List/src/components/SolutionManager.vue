7299889bff86cbc0b9f51bc6533a92517802beb2
yyy5044 <yyy5044@naver.com>
2026-03-19 16:46:09 +0900

    Solution Manager 컴포넌트 추가

---
+++ Algo-List/src/components/SolutionManager.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+  selectedItem: Object
+})
+</script>
+
+<template>
+  <div class="solution-manager">
+    <h3>풀이 관리</h3>
+    <p v-if="selectedItem">{{ selectedItem.title }}의 풀이 목록이 여기에 표시됩니다.</p>
+  </div>
+</template>
+
+<style scoped>
+.solution-manager {
+  border-top: 1px solid #eee;
+  padding: 20px 0;
+}
+
+.solution-manager h3 {
+  font-size: 16px;
+  color: #333;
+  margin-bottom: 12px;
+}
+</style>
