b841ffa79ee481f4496c4772382b50617abb1f9c
hapo <yyy5044@naver.com>
2026-06-05 17:08:30 +0900

    feat: UserProblemDto 추가

---
+++ backend/src/main/java/com/algolist/backend/problem/UserProblemDto.java
@@ -0,0 +1,18 @@
+package com.algolist.backend.problem;
+
+
+import java.time.LocalDate;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserProblemDto {
+	private Long userProblemId;
+	private Long userId;
+	private String grade;
+	private Integer solveCount;
+	private LocalDate lastSolvedDate;
+	private ProblemDto problem;
+}
