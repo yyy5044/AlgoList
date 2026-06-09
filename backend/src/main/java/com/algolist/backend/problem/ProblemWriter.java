8b6f849a2fc39b711cf0fb66fcbe818d5667b930
hapo <yyy5044@naver.com>
2026-06-09 22:14:44 +0900

    fix: 내부호출로 트랜잭션 호출 안 되는 현상 -> ProblemWriter 클래스 추가로 해결

---
+++ backend/src/main/java/com/algolist/backend/problem/ProblemWriter.java
@@ -0,0 +1,26 @@
+package com.algolist.backend.problem;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class ProblemWriter {
+
+    private final ProblemDao dao;
+
+    
+    @Transactional
+    public void save(ProblemDto dto) {
+        dao.insertProblem(dto);
+
+        if (dto.getProblemId() != null && dto.getProblemId() > 0
+                && dto.getCategory() != null) {
+            for (String cat : dto.getCategory()) {
+                dao.insertCategory(dto.getProblemId(), cat);
+            }
+        }
+    }
+}
